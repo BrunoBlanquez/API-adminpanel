683703ecfc64c76760c9fecfd8f8609a7c973db0
Bruno <b.blanquez@yahoo.com.br>
2022-06-14 18:02:14 -0300

    alterando tipo do preco de Int64g para Number

--- src/models/produtos.js
+++ src/models/produtos.js
@@ -6,7 +6,7 @@ const ProdutosSchema = new mongoose.Schema(
     nome: { type: String },
     imagem: { type: String },
     estoque: { type: Number },
-    preco: { type: Int64 },
+    preco: { type: Number },
     marca: { type: String }
   },
    {versionKey: 'Versão' },
