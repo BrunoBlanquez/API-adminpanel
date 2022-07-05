0cbcf1098ffdc5c04d86cf7d61b20e608a15e083
Bruno <b.blanquez@yahoo.com.br>
2022-07-05 16:51:34 -0300

    adicionando pasta config

--- src/models/Transacoes.js
+++ src/models/Transacoes.js
@@ -6,7 +6,7 @@ const TransacoesSchema = new mongoose.Schema(
     data: { type: Date, default: Date.now, required: true },
     produto: { type: mongoose.SchemaTypes.ObjectId, ref: 'produtos', required: true },
     comprador: { type: mongoose.SchemaTypes.ObjectId, ref: 'usuarios', required: true },
-    status: { type: String, required: true }
+    status: { type: String }
   },
    {versionKey: 'Versão' }
 )
